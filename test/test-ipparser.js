04223b56856aad448e4e8e9da7d417d52193e09c
Rajesh Segu <rajesh.segu@gmail.com>
2015-07-12 10:45:03 -0700

    better ip parser validation

--- test/test-ipparser.js
+++ test/test-ipparser.js
@@ -1,10 +1,11 @@
 var IPParser = require('../lib/IPParser');
 var scotchTape = require('scotch-tape');
 
-function verify(t, ip) {
+function verify(t, ip, expected) {
   t.doesNotThrow(function validate() {
     IPParser(ip);
   });
+  t.looseEquals(IPParser(ip), expected);
 }
 
 function verifyThrows(t, ip) {
@@ -16,8 +17,8 @@ function verifyThrows(t, ip) {
 var ipParseTest = scotchTape();
 ipParseTest('IPv4', function run(it) {
   it('should successfully parse correct formats', function should(t) {
-    verify(t, '127:0:0:1');
-    verify(t, '10:10:200:59');
+    verify(t, '127.0.0.1', new Buffer([0x7f, 0x00, 0x00, 0x01]));
+    verify(t, '10.10.200.59', new Buffer([0x0a, 0x0a, 0xc8, 0x3b]));
     t.end();
   });
 
@@ -30,8 +31,8 @@ ipParseTest('IPv4', function run(it) {
 
 ipParseTest('IPv6', function run(it) {
   it('should successfully parse correct formats', function should(t) {
-    verify(t, '2001:0db8:85a3:0000:0000:8a2e:0370:7334');
-    verify(t, '2001:db8:85a3::8a2e:370:7334');
+    verify(t, '2001:0db8:85a3:0000:0000:8a2e:0370:7334', new Buffer([0x20, 0x01, 0x0d, 0xb8, 0x85, 0xa3, 0x00, 0x00, 0x00, 0x00, 0x8a, 0x2e, 0x03, 0x70, 0x73, 0x34]));
+    verify(t, '2001:db8:85a3::8a2e:370:7334', new Buffer([0x20, 0x01, 0x0d, 0xb8, 0x85, 0xa3, 0x00, 0x00, 0x00, 0x00, 0x8a, 0x2e, 0x03, 0x70, 0x73, 0x34]));
     t.end();
   });
 
